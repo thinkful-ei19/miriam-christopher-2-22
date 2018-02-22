f77df6407aa1d28f25f7ad7cfd9eebb7a947c3b8
Christopher <cjszk@hotmail.com>
2018-02-22 10:48:55 -0800

    finished morning drills

---
+++ fizzBuzz/index.js
@@ -0,0 +1,38 @@
+//3 = fizz, 5 = buzz, fizzbuzz if both. otherwise numbers. starting from 0.
+$(function() {
+    function handleSubmit() {
+        $('form :submit').on('click', function() {
+            $('.js-results').html(' ');
+            let number = $('#number-choice').val();
+            console.log(number);
+            event.preventDefault();
+            for (i=0; i<=number; i++) {
+                if (i % 15 === 0) {
+                    //fizzbuzz
+                    $('.js-results').append(`
+                    <span class="fizz-buzz-item fizzbuzz">fizzbuzz</span>
+                    `);
+                }
+                else if (i % 3 === 0) {
+                    //fizz
+                    $('.js-results').append(`
+                    <span class="fizz-buzz-item fizz">fizz</span>
+                    `);
+                }
+                else if (i % 5 === 0) {
+                    //buzz
+                    $('.js-results').append(`
+                    <span class="fizz-buzz-item buzz">buzz</span>
+                    `);
+                }
+                else {
+                    //number
+                    $('.js-results').append(`
+                    <span class="fizz-buzz-item">' + i + '</span>
+                    `);
+                }
+            }
+            event.stopPropagation();
+        })
+    }
+})
